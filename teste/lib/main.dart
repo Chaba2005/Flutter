120dbd0db585c461da19ab5a38f364191b097506
Chatheus <rtromero.sp@gmail.com>
2023-03-01 22:02:34 -0300

    Alterações

--- teste/lib/main.dart
+++ teste/lib/main.dart
@@ -3,6 +3,8 @@
 //conjunto de pixels = widget = classe
 //widget Center() - para centralizar, usamos child: para definir como widget filho
 //widget Text() - para texto, usa-se um parametro TextDirection: TextDirection.algumaCoisa
+//widgets com PAGE ocupam basicamente a tela inteira
+//Scaffold usa BODY nao CHILD
 import 'package:flutter/material.dart';
 
 void main(){
@@ -14,8 +16,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(child: Text('Yeyyyy',textDirection: TextDirection.rtl,)),
+      home: HomePage(),
     );//como um body do HTML, so usaremos ele uma vez
   } 
+
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( //barra de manu basicamente
+        title: Text('Home'),
+      ),
+      body: Center(
+          child: Text('Yeyyyy')
+          ),
+    );
+  }
 
 }
